01a38b90f2a6015bffcf916d9308649fd4e15563
amakovey <46588607+amakovey@users.noreply.github.com>
2022-01-04 16:58:55 +0400

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_button(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+    button = browser.find_element_by_class_name("btn.btn-lg.btn-primary")
+    assert button is not None
+
