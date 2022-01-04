058db6d3fda604c56b59d59de4e0b36a043a00e6
amakovey <46588607+amakovey@users.noreply.github.com>
2022-01-04 17:07:48 +0400

    assert updated

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_button(browser):
     browser.get(link)
     browser.implicitly_wait(10)
-    button = browser.find_element_by_class_name("btn.btn-lg.btn-primary")
-    assert button is not None
+    button = browser.find_elements_by_class_name("btn.btn-lg.btn-primary")
+    assert button, 'Button is not found'
 
